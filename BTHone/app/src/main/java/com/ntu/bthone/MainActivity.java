25c013ef9484376f612bc64bb030eba72206c059
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-03-08 08:34:26 +0700

    Up xử lý nút back

--- BTHone/app/src/main/java/com/ntu/bthone/MainActivity.java
+++ BTHone/app/src/main/java/com/ntu/bthone/MainActivity.java
@@ -2,6 +2,7 @@ package com.ntu.bthone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void changeToEx_1(View view) {
         setContentView(R.layout.ex_1);
     }
@@ -25,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.ex_2);
     }
 
-
-
     public void changeToEX_3(View view) {
         setContentView(R.layout.ex_3);
 
@@ -34,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
         EditText editTextB = findViewById(R.id.edit_B_ex3);
         EditText viewKetQua = findViewById(R.id.viewKq);
         Button btnXLC = findViewById(R.id.btn_Ex3);
+    }
+
+    public void backEx(View View){
+        Button backEx_3 = findViewById(R.id.btn3_back);
+        Button backEx_1 = findViewById(R.id.btn_back1);
+        Button backEx_2 = findViewById(R.id.btn_back2);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     public void XulyCong(View view) {
