f80fdfae4a64d44b0765ed0a07e6818036d7c35c
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-03-15 02:45:39 +0700

    Fix up UI thêm xóa

--- LoginApp/app/src/main/java/com/ntu/loginapp/HomeActivity.java
+++ LoginApp/app/src/main/java/com/ntu/loginapp/HomeActivity.java
@@ -1,14 +1,36 @@
 package com.ntu.loginapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageButton add;
+    AlertDialog dialog;
+    LinearLayout layout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        add = findViewById(R.id.txtAdd);
+        layout = findViewById(R.id.container);
+
+        
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
 
     }
 }
