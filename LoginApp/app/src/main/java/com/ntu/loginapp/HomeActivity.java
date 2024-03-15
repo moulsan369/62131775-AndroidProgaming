7df449cc9427d59c2fe6ed12a92c4c9ce8425658
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-03-15 10:11:03 +0700

    Fix sử dụng Builder cho trang homepage

--- LoginApp/app/src/main/java/com/ntu/loginapp/HomeActivity.java
+++ LoginApp/app/src/main/java/com/ntu/loginapp/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.ntu.loginapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,7 +27,7 @@ public class HomeActivity extends AppCompatActivity {
         add = findViewById(R.id.txtAdd);
         layout = findViewById(R.id.container);
 
-        
+        buildDialog();
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +35,43 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void buildDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this );
+        View view = getLayoutInflater().inflate(R.layout.dialog, null);
+        EditText name = view.findViewById(R.id.txtEdit);
+        builder.setView(view);
+        builder.setTitle("Điền tên")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        addlist(name.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        dialog = builder.create();
+    }
+    private void addlist(String name){
+        View view = getLayoutInflater().inflate(R.layout.list_item, null);
+
+        TextView nameView = view.findViewById(R.id.txtName);
+        ImageButton delete = view.findViewById(R.id.btnDelete);
+
+        nameView.setText(name);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+
+        layout.addView(view);
     }
 }
 
