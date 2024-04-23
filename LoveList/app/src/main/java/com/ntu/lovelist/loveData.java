e9aaa7869f986f0a796ea8305c04715d0305c2d7
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-04-23 13:43:35 +0700

    make holder love

---
+++ LoveList/app/src/main/java/com/ntu/lovelist/loveData.java
@@ -0,0 +1,16 @@
+package com.ntu.lovelist;
+
+public class loveData {
+    String name;
+    String date;
+    String message;
+
+    loveData(String name,
+             String date,
+             String message)
+    {
+        this.name = name;
+        this.date = date;
+        this.message = message;
+    }
+}
---
+++ LoveList/app/src/main/java/com/ntu/lovelist/loveViewHolder.java
@@ -0,0 +1,27 @@
+package com.ntu.lovelist;
+import android.view.View;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class loveViewHolder extends RecyclerView.ViewHolder {
+    TextView txtName;
+    TextView txtMessage;
+    TextView txtDate;
+    View view;
+
+    loveViewHolder(View itemView)
+    {
+        super(itemView);
+        txtName
+                = (TextView)itemView
+                .findViewById(R.id.txtName);
+        txtDate
+                = (TextView)itemView
+                .findViewById(R.id.examDate);
+        txtMessage
+                = (TextView)itemView
+                .findViewById(R.id.examMessage);
+        view = itemView;
+    }
+}
+
