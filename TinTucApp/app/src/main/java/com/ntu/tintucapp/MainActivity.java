fe56c79e407c362002e40a4503246d921b53644e
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-04-26 12:03:28 +0700

    fix main đồng bộ chạy

--- TinTucApp/app/src/main/java/com/ntu/tintucapp/MainActivity.java
+++ TinTucApp/app/src/main/java/com/ntu/tintucapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ntu.tintucapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NewsFragment()).commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        // By using switch we can easily get 
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.news) {
+            selectedFragment = new NewsFragment();
+        } else if (itemId == R.id.add) {
+            selectedFragment = new AddFragment();
+        } else if (itemId == R.id.user) {
+            selectedFragment = new UserFragment();
+        }
+        // It will help to replace the
+        // one fragment to other.
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
 }
